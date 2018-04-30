842b329ee8d06daee8eb70981066e21c4acc1965
zhaojingjing <guzhao593@163.com>
2018-04-30 17:40:46 +0800

    添加头部搜索组件

--- src/components/common/m-header.vue
+++ src/components/common/m-header.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    <header>基&nbsp;&nbsp;本&nbsp;&nbsp;导&nbsp;&nbsp;航</header>
+  <div class="m-header">
+    <h1>基&nbsp;&nbsp;本&nbsp;&nbsp;导&nbsp;&nbsp;航</h1>
+    <m-search class="m-search"></m-search>
   </div>
 </template>
 
 <script>
+import MSearch from './m-search.vue'
 export default {
   name: 'MHeader',
+  components: {
+    MSearch
+  },
   data () {
     return {}
   }
@@ -14,12 +19,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  header{
-    height: 80px;
-    line-height: 80px;
-    color: #2cb314;
-    font-size: 24px;
-    font-weight: bold;
-    // font-family: 'Courier New', Courier, monospace;
+  .m-header{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    h1{
+      height: 80px;
+      line-height: 80px;
+      color: #2cb314;
+      font-size: 24px;
+      font-weight: bold;
+      flex: auto;
+    }
+    .m-search{
+      flex: auto;
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+      align-items: center;
+    }
   }
+  
 </style>
---
+++ src/components/common/m-search.vue
@@ -0,0 +1,73 @@
+<template>
+  <el-form>
+    <el-form-item style="width: 30vw">
+      <el-input type="text" v-model="word">
+        <el-select v-model="searchClass" slot="prepend">
+          <el-option
+            v-for="(item, idx) of searchClassOptions"
+            :key="idx"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      <el-button slot="append" @click="submit">搜索</el-button>
+      </el-input>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+  import req from 'api'
+
+  export default {
+    name: 'MSearch',
+    props: {
+      searchClass: {
+        type: String,
+        default: 'baidu'
+      }
+    },
+    data () {
+      return {
+        searchClassOptions: [
+          {
+            label: '百度',
+            value: 'baidu'
+          },
+          {
+            label: '站内',
+            value: 'inside'
+          }
+        ],
+        word: ''
+      }
+    },
+    methods: {
+      submit () {
+        if (this.searchClass === 'baidu') {
+          console.log(1111)
+          window.open(`http://baidu.com/s?word=${this.word}`)
+        } else {
+          req('')
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .el-form-item{
+    margin-bottom: 0px;
+    /deep/ .el-input__inner{
+      height: 30px;
+      line-height: 30px;
+      border-color: #dcdfe6 !important;
+    }
+    /deep/ .el-select .el-input{
+      width: 80px;
+      .el-input__inner{
+        border: 0 none;
+      }
+    }
+  }
+</style>
