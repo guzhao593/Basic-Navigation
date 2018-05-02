9ab3c74c7fed204f5a521d0631b987b4c965947e
zhaojingjing <guzhao593@163.com>
2018-05-02 21:15:56 +0800

    添加菜单编辑功能

--- src/components/common/m-menu.vue
+++ src/components/common/m-menu.vue
@@ -11,15 +11,19 @@
 				v-for="(name, idx) in menuData"
 				:key="idx"
 				:index="'/' + name.className"
+				style="padding-left: 28px"
 			>
 				<router-link :to="'/' + name.className" tag="div">
 					{{name.className}}
 				</router-link>
 			</el-menu-item>
-			<el-menu-item :index="'/ueditor'">
-				<router-link to="/ueditor" tag="div">ueditor</router-link>
-			</el-menu-item>
 		</draggable>
+		<transition>
+			<div class="edit-bar" :style="{top: editTop}">
+				<i class="el-icon-plus setting add" @click="handlerAdd()"></i>
+				<i class="el-icon-setting setting edit" @click="handlerEdit()"></i>
+			</div>
+		</transition>
   </el-menu>
 </template>
 
@@ -30,6 +34,9 @@
 	  name: 'MMenu',
 	  components: {
 	    draggable
+	  },
+	  props: {
+	    editTop: String
 	  },
 	  data () {
 	    return {
@@ -52,8 +59,34 @@
 </script>
 
 <style lang="scss" scoped>
+	@import 'style/var.scss';
 	.el-menu{
 		border-right: none;
+		position: relative;
+		&:hover .edit-bar{
+			display: flex;
+		}
+		.edit-bar{
+			transition: all .3s;
+			position: absolute;
+			left: 0;
+			display: none;
+			flex-direction: column;
+			justify-content: space-between;
+			background-color: #ccc;
+			opacity: 1;
+			padding: 5px 3px 0 5px;
+			border-bottom-right-radius: 8px;
+			.setting{
+				margin-bottom: 5px;
+			}
+			.add{
+				color: $button-color;
+			}
+			.edit{
+				color: #9d00ff;
+			}
+		}
 	}
 	.el-menu-item{
 		font-size: 12px;
--- src/components/home.vue
+++ src/components/home.vue
@@ -4,8 +4,8 @@
       <m-header></m-header>
     </el-header>
     <el-container>
-      <el-aside style="width: 10vw">
-        <m-menu></m-menu>
+      <el-aside style="width: 10vw" @scroll.native="getScroll">
+        <m-menu :edit-top="editTop"></m-menu>
       </el-aside>
       <el-main>
         <m-breadcrumb></m-breadcrumb>
@@ -23,6 +23,7 @@
     name: 'home',
     data () {
       return {
+        editTop: '0px'
       }
     },
     components: {
@@ -31,18 +32,39 @@
       MBreadcrumb
     },
     created () {
-      console.log(this.$route)
+    },
+    methods: {
+      getScroll (event) {
+        this.editTop = event.target.scrollTop + 'px'
+      }
     }
   }
 </script>
 
 <style scoped lang="scss">
+	@import 'style/var.scss';
   .el-aside{
     min-width: 100px;
     max-width: 200px;
     height: calc(100vh - 80px);
     background-color: #fff;
     border-right: 1px solid #e6e6e6;
+    position: relative;
+		.edit-bar{
+			position: absolute;
+			// top: 0;
+			left: 0;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			background-color: $main-color;
+			opacity: 0.8;
+			padding: 5px 0 0;
+      z-index: 999;
+			.setting{
+				margin-bottom: 5px;
+			}
+		}
   }
   .el-header{
     height: 120px;
